10e1b5c8ff28e2179e5ac9ae718f8f7f33ea70d4
Josh Rufin <joshrufin@gmail.com>
2017-04-19 08:53:43 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+var subtotal = 0;
+var total = 0;
+var groceryList = [
+  {
+    name: "Bread",
+    price: 1.89
+  },
+  {
+    name: "Cheese",
+    price: 1.25
+  },
+  {
+    name: "Ice Cream",
+    price: 3.00
+  },
+  {
+    name: "Cookies",
+    price: 3.10
+  },
+  {
+    name: "Chips",
+    price: 3.25
+  },
+  {
+    name: "Milk",
+    price: 2.08
+  },
+  {
+    name: "Granola",
+    price: 1.73
+  },
+  {
+    name: "Soda",
+    price: 4.25
+  },
+  {
+    name: "Apple Jacks",
+    price: 3.35
+  },
+  {
+    name: "Two Hearted Ale",
+    price: 9.97
+  },
+  ];
+groceryList.forEach(function(i) {
+  if (groceryList.price === 0) {
+    subtotal = i.price * 0.006;
+  }
+  console.log(i.name);
+  console.log(i.price);
+  total += i.price;
+});
+
+console.log("Total is " +total.toFixed(2));
